d6e4b7a2231e55eca1f3033eee76c1a8e0f2150a
Ryan Slater <ryan.j.slater.2@gmail.com>
2022-10-03 22:39:35 -0400

    Added github url

--- test.py
+++
@@ -1,8 +0,0 @@
-from src.slater_dl_wrapper.models import MultiLayerPerceptron
-import torch
-
-
-if __name__ == '__main__':
-    model = MultiLayerPerceptron(num_features=28**2, num_classes=10, hidden_sizes=[50, 20], activations=['ReLU'], flatten=True)
-    print(model)
-    print(dir(torch.nn.functional))
