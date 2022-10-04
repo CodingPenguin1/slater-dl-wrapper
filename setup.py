6b113bf60ebe5f197376fea87ccd5bdc0530e364
Ryan Slater <ryan.j.slater.2@gmail.com>
2022-10-03 22:36:30 -0400

    added github url

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ import setuptools
 
 setuptools.setup(
     name='slater_dl_wrapper',
-    version='0.0.1',
+    version='0.0.2',
     author='Ryan Slater',
     description='A wrapper for PyTorch to make it easier to train and evaluate models',
-    packages=['slater_dl_wrapper']
+    packages=['slater_dl_wrapper'],
+    url='https://github.com/CodingPenguin1/slater-dl-wrapper'
 )
